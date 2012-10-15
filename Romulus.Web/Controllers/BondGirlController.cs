98a9cec20a838d8c54882fdc5295afc4da2d6d87
Madhon <madhon@madhon.co.uk>
2012-10-15 16:39:31 +0100

    Add test project, update packages
    Add razor view for bond girl page

---
+++ Romulus.Web/Controllers/BondGirlController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace Romulus.Web.Controllers
+{
+    public class BondGirlController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
