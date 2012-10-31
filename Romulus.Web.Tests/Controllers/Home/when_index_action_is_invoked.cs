fae2f1daa0892a6f563b43d664e83f3d5e60c559
Madhon <madhon@madhon.co.uk>
2012-10-31 12:18:00 +0000

    Changed from xunit to MSpec
    updated knockoutjs

---
+++ Romulus.Web.Tests/Controllers/Home/when_index_action_is_invoked.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using FluentAssertions;
+using Machine.Specifications;
+using Romulus.Web.Controllers;
+
+namespace Romulus.Web.Tests.Controllers.Home
+{
+    [Subject(typeof (HomeController))]
+    public class when_index_action_is_invoked
+    {
+        private static HomeController controller;
+        private static ViewResult viewResult;
+
+        private Establish context = () => controller = new HomeController();
+
+        private Because of = () => viewResult = (ViewResult) controller.Index();
+
+        private It should_return_a_ViewResult_with_default_view_name = () => viewResult.ViewName.Should().BeEmpty();
+    }
+}
