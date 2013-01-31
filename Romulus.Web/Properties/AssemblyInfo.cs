f97407a8947137e258d2a84c96c72b594a8c6c49
Madhon <madhon@madhon.co.uk>
2013-01-31 08:41:56 +0000

    Updated AutoFac

--- Romulus.Web/Properties/AssemblyInfo.cs
+++ Romulus.Web/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Madhon")]
 [assembly: AssemblyProduct("Romulus.Web")]
-[assembly: AssemblyCopyright("Copyright © Madhon 2012")]
+[assembly: AssemblyCopyright("Copyright © Madhon 2012-2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -18,4 +18,4 @@ using System.Runtime.InteropServices;
 [assembly: Guid("0d8551c1-6233-46b0-98e6-1a972e19a7a9")]
 
 [assembly: AssemblyVersion("4.0.0.0")]
-[assembly: AssemblyFileVersion("4.0.13023.1530")]
+[assembly: AssemblyFileVersion("4.0.13031.0839")]
