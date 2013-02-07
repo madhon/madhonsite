09d33b51efcca8e3be6c672a78e598cdd101c4bb
Madhon <madhon@madhon.co.uk>
2013-02-07 09:46:14 +0000

    Updated jquery and jquery validation

--- Romulus.Web/Scripts/_references.js
+++ Romulus.Web/Scripts/_references.js
@@ -1,4 +1,4 @@
-﻿/// <reference path="jquery-1.9.0.js" />
+﻿/// <reference path="jquery-1.9.1.js" />
 /// <reference path="jquery.validate.js" />
 /// <reference path="jquery.validate.unobtrusive.js" />
-/// <reference path="knockout-2.1.1.debug.js" />
+/// <reference path="knockout-2.2.1.debug.js" />
