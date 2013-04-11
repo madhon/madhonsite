f8699ee5cd280fa1f4e34724a4c7a8bfc0bd69de
Madhon <madhon@madhon.co.uk>
2013-04-11 16:13:16 +0100

    updated packages

--- Romulus.Web/Properties/AssemblyInfo.cs
+++ Romulus.Web/Properties/AssemblyInfo.cs
@@ -18,4 +18,4 @@ using System.Runtime.InteropServices;
 [assembly: Guid("0d8551c1-6233-46b0-98e6-1a972e19a7a9")]
 
 [assembly: AssemblyVersion("4.0.0.0")]
-[assembly: AssemblyFileVersion("4.0.13063.1654")]
+[assembly: AssemblyFileVersion("4.0.13101.1611")]
