329d1fcfd2b90f496002eaf7d31790baa19f05e4
Madhon <madhon@madhon.co.uk>
2013-05-22 16:22:24 +0100

    better bump version number

--- Romulus.Web/Properties/AssemblyInfo.cs
+++ Romulus.Web/Properties/AssemblyInfo.cs
@@ -18,4 +18,4 @@ using System.Runtime.InteropServices;
 [assembly: Guid("0d8551c1-6233-46b0-98e6-1a972e19a7a9")]
 
 [assembly: AssemblyVersion("4.0.0.0")]
-[assembly: AssemblyFileVersion("4.0.13140.1741")]
+[assembly: AssemblyFileVersion("4.1.13142.1621")]
