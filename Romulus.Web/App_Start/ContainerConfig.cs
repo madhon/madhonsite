a87d753e6a820e4e8ad434c78a30b87530f795cd
Madhon <madhon@madhon.co.uk>
2013-06-26 16:24:49 +0100

    autofac support in glimpse

--- Romulus.Web/App_Start/ContainerConfig.cs
+++ Romulus.Web/App_Start/ContainerConfig.cs
@@ -19,6 +19,8 @@ namespace Romulus.Web
             builder.RegisterModule<RomulusSiteModule>();
 
             var container = builder.Build();
+            container.ActivateGlimpse();
+
             DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
         }
 
