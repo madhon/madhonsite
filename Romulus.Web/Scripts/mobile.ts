19d94d11bfa3d97b3702ca78a9198b939f36157c
Madhon <madhon@madhon.co.uk>
2013-07-01 16:15:16 +0100

    mobile.ts

---
+++ Romulus.Web/Scripts/mobile.ts
@@ -0,0 +1,6 @@
+﻿/// <reference path="typings/jquery/jquery.d.ts" />
+/// <reference path="typings/jquerymobile/jquerymobile.d.ts" />
+         
+$(document).ready(function () {
+    $.mobile.ajaxEnabled = false; 
+});
