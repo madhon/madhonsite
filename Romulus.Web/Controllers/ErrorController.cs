99116756ab7c3054c435136789486d6376df174c
Madhon <madhon@madhon.co.uk>
2013-07-03 16:59:36 +0100

    add error pages

--- Romulus.Web/Controllers/ErrorController.cs
+++ Romulus.Web/Controllers/ErrorController.cs
@@ -2,17 +2,21 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Mvc;
-using Romulus.Web.Infrastructure;
 
 namespace Romulus.Web.Controllers
 {
     public class ErrorController : Controller
     {
-        [Route("404")]
+        public ActionResult General()
+        {
+            Response.StatusCode = 500;
+            return View("General");
+        }
+
         public ActionResult PageNotFound()
         {
             Response.StatusCode = 404;
-            return new ContentResult {Content = "Not Found (this is a 404 page)"};
+            return View("PageNotFound");
         }
 
     }
