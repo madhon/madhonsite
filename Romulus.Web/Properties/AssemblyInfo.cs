b1900333d44a61426ad3360c16179bb99a38d2d8
Madhon <madhon@madhon.co.uk>
2013-12-11 11:58:46 +0000

    add stamp.fody

--- Romulus.Web/Properties/AssemblyInfo.cs
+++ Romulus.Web/Properties/AssemblyInfo.cs
@@ -16,6 +16,6 @@ using System.Runtime.InteropServices;
 [assembly: Guid("0d8551c1-6233-46b0-98e6-1a972e19a7a9")]
 
 [assembly: AssemblyVersion("4.0.0.0")]
-[assembly: AssemblyFileVersion("4.1.13343.1138")]
+[assembly: AssemblyFileVersion("4.1.13345.1158")]
 //[assembly: AssemblyInformationalVersion("${major}.${minor}.${patch}-${branch} ${hash|short}")]
-[assembly: AssemblyInformationalVersion("4.1.13343.1138.0-master 3fcfbb8")]
+[assembly: AssemblyInformationalVersion("4.1.13345.1158-%branch% %githash%")]
