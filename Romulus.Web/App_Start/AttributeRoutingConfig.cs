a8ab3a8bff0bee5ddb6ff26098fa977f6608c629
Madhon <madhon@madhon.co.uk>
2014-02-14 13:39:59 +0000

    change to google cdn for jquery

--- Romulus.Web/App_Start/AttributeRoutingConfig.cs
+++ Romulus.Web/App_Start/AttributeRoutingConfig.cs
@@ -8,7 +8,13 @@ namespace Romulus.Web
     public static class AttributeRoutingConfig
 	{
 		public static void RegisterRoutes(RouteCollection routes) 
-		{    
+		{   
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            routes.IgnoreRoute("{*favicon}", new {favicon = @"(.*/)?favicon.ico(/.*)?"});
+            routes.Ignore("bond_girl.php");
+            routes.Ignore("exceptions.axd");
+            routes.Ignore("MvcDiagnostics.aspx");
+            routes.Ignore("mvcdiagnostics.aspx");
 			routes.MapAttributeRoutes();
 		}
 
