fcc82d6ce4f929be604393cbe80504e1117c0250
Jonathon Douglas <jdouglas@acquisinsurance.com>
2015-07-09 09:28:56 +0100

    Change from jshint to eslint Modified layouts slightly

--- Romulus.Web/Content/js/app-mobile.js
+++ Romulus.Web/Content/js/app-mobile.js
@@ -1,3 +1,8 @@
-$(document).ready(function() {
-  $(".button-collapse").sideNav();
-});
+(function($, window, document) {
+  'use strict';
+
+  $(function() {
+    $('.button-collapse').sideNav();
+  });
+
+})(jQuery, window, document, undefined);
