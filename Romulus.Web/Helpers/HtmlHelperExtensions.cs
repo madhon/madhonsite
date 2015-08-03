e83b9c537e3a771903981a0c90bd79238fc1d74c
Jonathon Douglas <jdouglas@acquisinsurance.com>
2015-08-03 16:22:50 +0100

    testing helper extensions

---
+++ Romulus.Web/Helpers/HtmlHelperExtensions.cs
@@ -0,0 +1,17 @@
+﻿namespace Romulus.Web.Helpers
+{
+  using Nancy.ViewEngines.Razor;
+
+  public static class HtmlHelperExtensions
+  {
+    public static IHtmlString CssTag<T>(this HtmlHelpers<T> helper, string uri)
+    {
+      return new NonEncodedHtmlString(string.Format("<link href=\"{0}\" rel=\"stylesheet\" />", uri));
+    }
+
+    public static IHtmlString JsTag<T>(this HtmlHelpers<T> helper, string uri, bool async = false)
+    {
+      return new NonEncodedHtmlString(string.Format("<script {1}type=\"text/javascript\" src=\"{0}\"></script>  ", uri, async ? "async " : string.Empty));
+    }
+  }
+}
