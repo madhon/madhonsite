423b298d8dca7b0531d262ded99e3635225b7983
Jonathon Douglas <jdouglas@acquisinsurance.com>
2015-09-22 11:55:13 +0100

    Updated Materialize url to v0.97.1

--- Romulus.Web/Constants/ContentDeliveryNetwork.cs
+++ Romulus.Web/Constants/ContentDeliveryNetwork.cs
@@ -33,9 +33,9 @@
       {
         public const string Domain = "cdnjs.cloudflare.com";
         public const string MaterializeCssUrl =
-          "//cdnjs.cloudflare.com/ajax/libs/materialize/0.97.0/css/materialize.min.css";
+          "//cdnjs.cloudflare.com/ajax/libs/materialize/0.97.1/css/materialize.min.css";
         public const string MaterializeJsUrl =
-          "//cdnjs.cloudflare.com/ajax/libs/materialize/0.97.0/js/materialize.min.js";
+          "//cdnjs.cloudflare.com/ajax/libs/materialize/0.97.1/js/materialize.min.js";
       }
     }
 }
