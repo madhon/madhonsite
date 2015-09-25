39491c94c1aeaf268532caef43728adf6cd6609e
Jonathon Douglas <jdouglas@acquisinsurance.com>
2015-09-25 15:20:57 +0100

    annotations in html helpers

--- Romulus.Web/Helpers/HtmlHelperExtensions.cs
+++ Romulus.Web/Helpers/HtmlHelperExtensions.cs
@@ -1,15 +1,16 @@
 ﻿namespace Romulus.Web.Helpers
 {
+  using JetBrains.Annotations;
   using Nancy.ViewEngines.Razor;
 
   public static class HtmlHelperExtensions
   {
-    public static IHtmlString CssTag<T>(this HtmlHelpers<T> helper, string uri)
+    public static IHtmlString CssTag<T>([UsedImplicitly] this  HtmlHelpers<T> helper, string uri)
     {
       return new NonEncodedHtmlString(string.Format("<link href=\"{0}\" rel=\"stylesheet\" />", uri));
     }
 
-    public static IHtmlString JsTag<T>(this HtmlHelpers<T> helper, string uri, bool async = false)
+    public static IHtmlString JsTag<T>([UsedImplicitly] this HtmlHelpers<T> helper, string uri, bool async = false)
     {
       return new NonEncodedHtmlString(string.Format("<script {1}type=\"text/javascript\" src=\"{0}\"></script>  ", uri, async ? "async " : string.Empty));
     }
