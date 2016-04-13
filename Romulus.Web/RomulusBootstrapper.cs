f08d085beb24b6cd5e5046534714e5291720338e
Jonathon Douglas <jdouglas@acquisinsurance.com>
2016-04-13 16:43:24 +0100

    Added Nancy.Gzip

--- Romulus.Web/RomulusBootstrapper.cs
+++ Romulus.Web/RomulusBootstrapper.cs
@@ -1,37 +1,42 @@
 ﻿namespace Romulus.Web
 {
-  using DryIoc;
-  using JetBrains.Annotations;
-  using Nancy.Bootstrapper;
-  using Nancy.Conventions;
-  using Nancy.Security;
-  using Romulus.Web.Services;
+    using System;
+    using DryIoc;
+    using JetBrains.Annotations;
+    using Nancy.Bootstrapper;
+    using Nancy.Conventions;
+    using Nancy.Gzip;
+    using Nancy.Security;
+    using Romulus.Web.Services;
 
-  [UsedImplicitly]
-  public class RomulusBootstrapper : DryIocNancyBootstrapper
-  {
-    protected override byte[] FavIcon => null;
-
-    protected override void ConfigureConventions(NancyConventions nancyConventions)
+    [UsedImplicitly]
+    public class RomulusBootstrapper : DryIocNancyBootstrapper
     {
-      base.ConfigureConventions(nancyConventions);
-      MobileViewLocationConventions.Enable(nancyConventions);
-    }
+        protected override byte[] FavIcon => null;
 
-    protected override void ConfigureApplicationContainer(IContainer existingContainer)
-    {
-      base.ConfigureApplicationContainer(existingContainer);
+        protected override void ConfigureConventions(NancyConventions nancyConventions)
+        {
+            base.ConfigureConventions(nancyConventions);
+            MobileViewLocationConventions.Enable(nancyConventions);
+        }
 
-      existingContainer.Register<ITransport, GmailTransport>();
-      existingContainer.Register<IContactService, ContactService>();
-    }
+        protected override void ConfigureApplicationContainer(IContainer existingContainer)
+        {
+            base.ConfigureApplicationContainer(existingContainer);
 
-    protected override void ApplicationStartup(IContainer container, IPipelines pipelines)
-    {
-      RomulsStatusCodeHandler.AddCode(404);
-      //CustomStatusCode.AddCode(ConfigurationManager.AppSettings["HttpErrorCodes"].Split(',').Select(x => int.Parse(x)));
-      Csrf.Enable(pipelines);
-      base.ApplicationStartup(container, pipelines);
+            existingContainer.Register<ITransport, GmailTransport>();
+            existingContainer.Register<IContactService, ContactService>();
+        }
+
+        protected override void ApplicationStartup(IContainer container, IPipelines pipelines)
+        {
+            RomulsStatusCodeHandler.AddCode(404);
+            //CustomStatusCode.AddCode(ConfigurationManager.AppSettings["HttpErrorCodes"].Split(',').Select(x => int.Parse(x)));
+            Csrf.Enable(pipelines);
+
+            pipelines.EnableGzipCompression();
+
+            base.ApplicationStartup(container, pipelines);
+        }
     }
-  }
 }
