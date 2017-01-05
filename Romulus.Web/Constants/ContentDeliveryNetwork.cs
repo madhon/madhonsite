54c6fff189405c1cbfa341447c062518b176776f
Jonathon Douglas <jdouglas@acquisinsurance.com>
2017-01-05 11:53:12 +0000

    Remote IE support v9 and earlier

--- Romulus.Web/Constants/ContentDeliveryNetwork.cs
+++ Romulus.Web/Constants/ContentDeliveryNetwork.cs
@@ -5,7 +5,6 @@ namespace Romulus.Web.Constants
         public static class Google
         {
             public const string Domain = "ajax.googleapis.com";
-            public const string JQuery1Url = "https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js";
             public const string JQuery2Url = "https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js";
             public const string JQuery3Url = "https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js";
             public const string SwfObjectUrl = "https://ajax.googleapis.com/ajax/libs/swfobject/2.2/swfobject.js";
@@ -23,8 +22,6 @@ namespace Romulus.Web.Constants
         {
             public const string Domain = "cdn.jsdelivr.net";
             public const string ModernizrUrl = "https://cdn.jsdelivr.net/modernizr/2.8.3/modernizr.min.js";
-            public const string RespondUrl = "https://cdn.jsdelivr.net/respond/1.4.2/respond.min.js";
-            public const string HtmlShivUrl = "https://cdn.jsdelivr.net/html5shiv/3.7.3/html5shiv-printshiv.min.js";
             public const string ParsleyUrl = "https://cdn.jsdelivr.net/parsleyjs/2.6.2/parsley.min.js";
         }
       
