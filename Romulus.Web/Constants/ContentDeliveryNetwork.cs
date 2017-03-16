2a3e3da5698c804360e42205c6ebc70a30c8d652
Jonathon Douglas <jdouglas@acquisinsurance.com>
2017-03-16 10:37:09 +0000

    Updated ParselyJS url

--- Romulus.Web/Constants/ContentDeliveryNetwork.cs
+++ Romulus.Web/Constants/ContentDeliveryNetwork.cs
@@ -13,27 +13,37 @@ namespace Romulus.Web.Constants
         public static class MaxCdn
         {
             public const string Domain = "maxcdn.bootstrapcdn.com";
-            public const string BootstrapCssUrl = "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css";
+
+            public const string BootstrapCssUrl =
+                "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css";
+
             public const string BootstrapJsUrl = "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js";
-            public const string FontAwesomeUrl = "https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css";
+
+            public const string FontAwesomeUrl =
+                "https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css";
         }
 
         public static class JsDelivr
         {
             public const string Domain = "cdn.jsdelivr.net";
+
+            public const string MaterializeCssUrl =
+                "https://cdn.jsdelivr.net/materialize/0.98.0/css/materialize.min.css";
+
+            public const string MaterializeJsUrl =
+                "https://cdn.jsdelivr.net/materialize/0.98.0/js/materialize.min.js";
+
             public const string ModernizrUrl = "https://cdn.jsdelivr.net/modernizr/2.8.3/modernizr.min.js";
-            public const string ParsleyUrl = "https://cdn.jsdelivr.net/parsleyjs/2.6.2/parsley.min.js";
+            public const string ParsleyUrl = "https://cdn.jsdelivr.net/parsleyjs/2.6.5/parsley.min.js";
         }
-      
+
         public static class Cloudflare
         {
             public const string Domain = "cdnjs.cloudflare.com";
-            public const string MaterializeCssUrl =
-                "https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.8/css/materialize.min.css";
-            public const string MaterializeJsUrl =
-                "https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.8/js/materialize.min.js";
+
             public const string Migrate1Url =
                 "https://cdnjs.cloudflare.com/ajax/libs/jquery-migrate/1.4.1/jquery-migrate.js";
+
             public const string Migrate3Url =
                 "https://cdnjs.cloudflare.com/ajax/libs/jquery-migrate/3.0.0/jquery-migrate.js";
         }
