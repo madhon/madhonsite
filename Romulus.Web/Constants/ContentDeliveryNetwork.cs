891cf17e4249a9e912f3b2abf507df41bdbc33d1
Madhon <madhon@madhon.co.uk>
2017-09-07 15:02:40 +0100

    Package and CDN updates Remove FluentValidation for now

--- Romulus.Web/Constants/ContentDeliveryNetwork.cs
+++ Romulus.Web/Constants/ContentDeliveryNetwork.cs
@@ -1,58 +1,55 @@
 ﻿namespace Romulus.Web.Constants
 {
-    public static class ContentDeliveryNetwork
+  public static class ContentDeliveryNetwork
+  {
+    public static class Google
     {
-        public static class Google
-        {
-            public const string Domain = "ajax.googleapis.com";
-            public const string JQuery2Url = "https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js";
-            public const string JQuery3Url = "https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js";
-            public const string SwfObjectUrl = "https://ajax.googleapis.com/ajax/libs/swfobject/2.2/swfobject.js";
-        }
-
-        public static class MaxCdn
-        {
-            public const string Domain = "maxcdn.bootstrapcdn.com";
-
-            public const string BootstrapCssUrl =
-                "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css";
-
-            public const string BootstrapJsUrl = "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js";
-
-            public const string FontAwesomeUrl =
-                "https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css";
-        }
-
-        public static class JsDelivr
-        {
-            public const string Domain = "cdn.jsdelivr.net";
-
-            public const string MaterializeCssUrl =
-                "https://cdn.jsdelivr.net/materialize/0.98.2/css/materialize.min.css";
-
-            public const string MaterializeJsUrl =
-                "https://cdn.jsdelivr.net/materialize/0.98.2/js/materialize.min.js";
-
-            public const string ModernizrUrl = "https://cdn.jsdelivr.net/modernizr/2.8.3/modernizr.min.js";
-            public const string ParsleyUrl = "https://cdn.jsdelivr.net/parsleyjs/2.7.1/parsley.min.js";
-        }
-
-        public static class Cloudflare
-        {
-            public const string Domain = "cdnjs.cloudflare.com";
-
-            public const string Migrate1Url =
-                "https://cdnjs.cloudflare.com/ajax/libs/jquery-migrate/1.4.1/jquery-migrate.js";
-
-            public const string Migrate3Url =
-                "https://cdnjs.cloudflare.com/ajax/libs/jquery-migrate/3.0.0/jquery-migrate.js";
-        }
-
-        public static class GetMdl
-        {
-            public const string MdlIconsUrl = "https://fonts.googleapis.com/icon?family=Material+Icons";
-            public const string MdlCssUrl = "https://code.getmdl.io/1.3.0/material.indigo-pink.min.css";
-            public const string MdlJsUrl = "https://code.getmdl.io/1.3.0/material.min.js";
-        }
+      public const string Domain = "ajax.googleapis.com";
+      public const string JQuery2Url = "https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js";
+      public const string JQuery3Url = "https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js";
+      public const string SwfObjectUrl = "https://ajax.googleapis.com/ajax/libs/swfobject/2.2/swfobject.js";
     }
+
+    public static class MaxCdn
+    {
+      public const string Domain = "maxcdn.bootstrapcdn.com";
+
+      public const string BootstrapCssUrl =
+        "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css";
+
+      public const string BootstrapJsUrl = "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js";
+
+      public const string FontAwesomeUrl =
+        "https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css";
+    }
+
+    public static class Cloudflare
+    {
+      public const string Domain = "cdnjs.cloudflare.com";
+
+
+      public const string MaterializeCssUrl =
+        "https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css";
+
+      public const string MaterializeJsUrl =
+        "https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js";
+
+      public const string ModernizrUrl = "https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js";
+
+      public const string ParsleyUrl = "https://cdnjs.cloudflare.com/ajax/libs/parsley.js/2.7.2/parsley.min.js";
+
+      public const string Migrate1Url =
+        "https://cdnjs.cloudflare.com/ajax/libs/jquery-migrate/1.4.1/jquery-migrate.js";
+
+      public const string Migrate3Url =
+        "https://cdnjs.cloudflare.com/ajax/libs/jquery-migrate/3.0.0/jquery-migrate.js";
+    }
+
+    public static class GetMdl
+    {
+      public const string MdlIconsUrl = "https://fonts.googleapis.com/icon?family=Material+Icons";
+      public const string MdlCssUrl = "https://code.getmdl.io/1.3.0/material.indigo-pink.min.css";
+      public const string MdlJsUrl = "https://code.getmdl.io/1.3.0/material.min.js";
+    }
+  }
 }
--- Romulus.Web/ViewModels/ContactViewModel.cs
+++ Romulus.Web/ViewModels/ContactViewModel.cs
@@ -1,16 +1,26 @@
 ﻿namespace Romulus.Web.ViewModels
 {
-    using System;
-    using JetBrains.Annotations;
-    using MediatR;
+  using System;
+  using System.Collections.Generic;
+  using System.ComponentModel.DataAnnotations;
+  using JetBrains.Annotations;
+  using MediatR;
 
-    [UsedImplicitly]
-    public class ContactViewModel : INotification
+  [UsedImplicitly]
+  public class ContactViewModel : INotification, IValidatableObject
+  {
+    [Required(AllowEmptyStrings = false, ErrorMessage = "Enter Your Name")]
+    public string Name { get; set; }
+
+    [EmailAddress(ErrorMessage = "Enter a valid email address")]
+    public string Email { get; set; }
+
+    [Required(AllowEmptyStrings = false, ErrorMessage = "Enter Your Name")]
+    public string Message { get; set; }
+
+    public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
     {
-        public string Name { get; set; }
-
-        public string Email { get; set; }
-
-        public string Message { get; set; }
+      throw new NotImplementedException();
     }
+  }
 }
