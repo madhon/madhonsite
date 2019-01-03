078c1a394e49bbdb97fec44ee08f2f27be63aa74
Jonathon Douglas <jdouglas@acquisinsurance.com>
2019-01-03 16:38:07 +0000

    Kestrel config changes and remove Serilog

--- Romulus.Web/Program.cs
+++ Romulus.Web/Program.cs
@@ -1,45 +1,19 @@
-﻿namespace Romulus.Web
+namespace Romulus.Web
 {
-    using System;
-    using Lamar.Microsoft.DependencyInjection;
-    using Microsoft.AspNetCore;
-    using Microsoft.AspNetCore.Hosting;
-    using Serilog;
-    using Serilog.Events;
+  using Lamar.Microsoft.DependencyInjection;
+  using Microsoft.AspNetCore;
+  using Microsoft.AspNetCore.Hosting;
 
-    public class Program
+  public class Program
+  {
+    public static void Main(string[] args) => CreateWebHostBuilder(args).Build().Run();
+
+    public static IWebHostBuilder CreateWebHostBuilder(string[] args)
     {
-        public static int Main(string[] args)
-        {
-            Log.Logger = new LoggerConfiguration()
-                .MinimumLevel.Debug()
-                .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
-                .Enrich.FromLogContext()
-                .WriteTo.Console()
-                .CreateLogger();
-
-            try
-            {
-                Log.Information("Starting Web Host");
-                CreateWebHostBuilder(args).Build().Run();
-                return 0;
-            }
-            catch (Exception ex)
-            {
-                Log.Fatal(ex, "Host Terminated unexpectedly");
-                return 1;
-            }
-            finally
-            {
-                Log.CloseAndFlush();
-            }
-        }
-
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseKestrel((bc, o) => { o.AddServerHeader = false; })
-                .UseLamar()
-                .UseStartup<Startup>()
-                .UseSerilog();
+      return WebHost.CreateDefaultBuilder(args)
+        .ConfigureKestrel((bc, o) => { o.AddServerHeader = false; })
+        .UseLamar()
+        .UseStartup<Startup>();
     }
+  }
 }
