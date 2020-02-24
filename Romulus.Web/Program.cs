17bc9debe44956f1d349e861bddb9411ff97ccfb
Madhon <madhon@madhon.com>
2020-02-24 09:06:30 +0000

    Fix RID and wwwroot (hopefully)

--- Romulus.Web/Program.cs
+++ Romulus.Web/Program.cs
@@ -1,16 +1,21 @@
 namespace Romulus.Web
 {
+  using System;
+  using System.IO;
   using Microsoft.AspNetCore.Hosting;
   using Microsoft.Extensions.Hosting;
 
   public class Program
   {
+    static string webRoot = Path.Combine(AppContext.BaseDirectory, "wwwroot");
+
     public static void Main(string[] args) => CreateHostBuilder(args).Build().Run();
 
     public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
+                    webBuilder.UseWebRoot(webRoot);
                     webBuilder.UseStartup<Startup>();
                 });
     
