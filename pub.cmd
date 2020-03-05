226fd75f83f32e7566a703757060334707cb2c1a
Madhon <madhon@madhon.com>
2020-03-05 10:54:15 +0000

    modifed pub.cmd slightly

--- pub.cmd
+++ pub.cmd
@@ -1,1 +1,8 @@
-dotnet publish -c release -r linux-x64 -o ..\madhonpub --self-contained=false
+@echo off
+dotnet clean --nologo -v m
+
+dotnet restore -v m
+
+dotnet build --nologo -v m --no-restore
+
+dotnet publish --nologo -v m -c release -r linux-x64 -o ..\madhonpub --self-contained=false --no-build
