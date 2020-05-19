a1183128e83a45541848f28e6b16888beeacc687
Madhon <madhon@madhon.com>
2020-05-19 11:21:49 +0100

    Fix .net 3.1.4 publish annoyance

--- pub.cmd
+++ pub.cmd
@@ -3,6 +3,6 @@ dotnet clean --nologo -v m
 
 dotnet restore -v m
 
-dotnet build --nologo -c release -r linux-x64 -v m --no-restore
+dotnet build --nologo -c release -v m --no-restore
 
-dotnet publish --nologo -v m -c release -r linux-x64 -o ..\madhonpub --no-build
+dotnet publish --nologo -v m -c release -o ..\madhonpub --self-contained=false --no-build
