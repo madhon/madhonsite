02b824b414505aa4cc199649be7893e8bd92e74a
Madhon <madhon@madhon.co.uk>
2020-10-23 10:38:44 +0100

    feat: BuildVer tag helper added

---
+++ Romulus.Web/Features/TagHelpers/BuildVerTagHelper.cs
@@ -0,0 +1,14 @@
+namespace Romulus.Web.Features.TagHelpers
+{
+	using Microsoft.AspNetCore.Razor.TagHelpers;
+
+	[HtmlTargetElement("BuildVer", TagStructure = TagStructure.NormalOrSelfClosing)]
+	public class BuildVerTagHelper : TagHelper
+	{
+		public override void Process(TagHelperContext context, TagHelperOutput output)
+		{
+			output.TagName = string.Empty;
+			output.Content.SetHtmlContent($"<!-- {ThisAssembly.AssemblyInformationalVersion} -->");
+		}
+	}
+}
