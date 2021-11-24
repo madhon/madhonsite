d0d4b9dc7039033afc78fd4efa04d00f68aa11d0
Madhon <madhon@madhon.co.uk>
2021-11-24 19:32:46 +0000

    chore: update cdn links and put .net core ver in comment

--- Romulus.Web/Features/TagHelpers/DotNetCoreVersionTagHelper.cs
+++ Romulus.Web/Features/TagHelpers/DotNetCoreVersionTagHelper.cs
@@ -1,22 +1,16 @@
 namespace Romulus.Web.Features.TagHelpers
 {
-  using System.Reflection;
-  using System.Runtime.Versioning;
-  using Microsoft.AspNetCore.Razor.TagHelpers;
+	using System.Runtime.InteropServices;
+	using Microsoft.AspNetCore.Razor.TagHelpers;
 
-  [HtmlTargetElement("DotNetCoreVersion", TagStructure = TagStructure.NormalOrSelfClosing)]
-  public class DotNetCoreVersionTagHelper : TagHelper
-  {
-    public override void Process(TagHelperContext context, TagHelperOutput output)
-    {
-      output.TagName = string.Empty;
-
-      var framework = Assembly
-        .GetEntryAssembly()?
-        .GetCustomAttribute<TargetFrameworkAttribute>()?
-        .FrameworkName;
-
-      output.Content.SetHtmlContent($"<!-- {framework} -->");
-    }
-  }
+	[HtmlTargetElement("DotNetCoreVersion", TagStructure = TagStructure.NormalOrSelfClosing)]
+	public class DotNetCoreVersionTagHelper : TagHelper
+	{
+		public override void Process(TagHelperContext context, TagHelperOutput output)
+		{
+			output.TagName = string.Empty;
+	  		var netcoreVer = RuntimeInformation.FrameworkDescription;
+			output.Content.SetHtmlContent($"<!-- {netcoreVer} -->");
+		}
+	}
 }
