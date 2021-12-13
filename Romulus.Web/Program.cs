2c41cb6cbd1c7cf17ee75a1eea3612670e69d2ca
Madhon <madhon@madhon.co.uk>
2021-12-13 12:14:55 +0000

    feat: Migrated to minimal apis for app startup

--- Romulus.Web/Program.cs
+++ Romulus.Web/Program.cs
@@ -1,21 +1,75 @@
-namespace Romulus.Web
+using System.Reflection;
+using FluentValidation.AspNetCore;
+using MediatR;
+using Microsoft.AspNetCore.HttpOverrides;
+using Microsoft.AspNetCore.Mvc;
+using Romulus.Web.Infrastructure;
+using Romulus.Web.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.WebHost.ConfigureKestrel(o => o.AddServerHeader = false);
+builder.Host.UseSystemd();
+
+
+// ConfigureServices migrated elements
+builder.Services.Configure<ForwardedHeadersOptions>(options =>
 {
-  using Microsoft.AspNetCore.Hosting;
-  using Microsoft.Extensions.Hosting;
+	options.KnownNetworks.Clear();
+	options.ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto;
+});
 
-  public class Program
-  {
-    public static void Main(string[] args) => CreateHostBuilder(args).Build().Run();
+builder.Services.AddCustomLogging(builder.Configuration, builder.Environment);
 
-    public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-	            .UseSystemd()
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.ConfigureKestrel((bc, o) => { o.AddServerHeader = false; })
-                      //.UseWebRoot(webRoot)
-                      .UseStartup<Startup>();
-                });
-    
-  }
-}
+builder.Services.AddAntiForgerySecurely(builder.Environment);
+
+builder.Services.AddRouting(options =>
+{
+	options.AppendTrailingSlash = true;
+	options.LowercaseUrls = true;
+});
+
+builder.Services.AddHealthChecks();
+
+builder.Services.AddResponseCaching();
+//services.AddResponseCompression(options => options.MimeTypes = ResponseCompressionMimeTypes.Defaults);
+
+builder.Services.AddMediatR(typeof(Program).GetTypeInfo().Assembly);
+
+builder.Services.AddControllersWithViews(opts => opts.Filters.Add(new AutoValidateAntiforgeryTokenAttribute()))
+	.AddFeatureFolders()
+	.AddFluentValidation(cfg =>
+	{
+		cfg.RegisterValidatorsFromAssemblyContaining<Program>();
+	});
+
+builder.Services.AddTransient<ITransport, NullTransport>();
+
+
+var app = builder.Build();
+
+// ConfigureApp migrated elements
+
+app.UseForwardedHeaders();
+
+app.IfDevelopment(app.Environment, a =>
+{
+	a.UseDeveloperExceptionPage();
+});
+
+app.UseStaticFilesWithCacheControl(app.Environment);
+
+app.UseRouting();
+
+app.UseResponseCaching();
+//app.UseResponseCompression();
+
+app.UseSecurityHeadersMiddleware(new SecurityHeadersBuilder().AddDefaultSecurePolicy());
+
+app.UseEndpoints(endpoints =>
+{
+	endpoints.MapHealthChecks("/healthz");
+	endpoints.MapDefaultControllerRoute();
+});
+
+app.Run();
