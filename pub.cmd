3f500b00f11c0effbfe267bb72901e3bba0a5285
Madhon <madhon@madhon.co.uk>
2022-02-14 10:18:27 +0000

    feat: build script testing

--- pub.cmd
+++ pub.cmd
@@ -3,6 +3,9 @@ dotnet clean --nologo -v m
 
 dotnet restore -v m
 
-dotnet build --configuration Release --no-restore
+dotnet build --configuration -c Release --runtime win10-x64 --self-contained true --no-restore -p:PublishSingleFile=true -p:ImportByWildcardBeforeSolution=false
 
-dotnet publish --no-restore --no-build --configuration Release --self-contained=false --output ..\madhonpub
+dotnet publish --runtime win10-x64  --self-contained true -c Release /p:PublishSingleFile=true -p:PublishTrimmed=True -p:TrimMode=CopyUsed --output ..\madhonpub
+
+
+rem dotnet publish --no-restore --no-build --configuration Release --self-contained=false --output ..\madhonpub
