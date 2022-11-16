074710092e2f338741a901b171691e14961e2e3c
Madhon <madhon@madhon.co.uk>
2022-11-16 17:04:26 +0000

    build: change trimming mode

--- pub.cmd
+++ pub.cmd
@@ -5,7 +5,7 @@ dotnet restore -v m
 
 dotnet build --configuration -c Release --runtime win10-x64 --self-contained true --no-restore -p:PublishSingleFile=true -p:ImportByWildcardBeforeSolution=false
 
-dotnet publish --runtime win10-x64  --self-contained true -c Release /p:PublishSingleFile=true -p:PublishTrimmed=True -p:TrimMode=CopyUsed --output ..\madhonpub
+dotnet publish --runtime win10-x64  --self-contained true -c Release /p:PublishSingleFile=true -p:PublishTrimmed=True -p:TrimMode=CopyUsed -p:_TrimmerDefaultAction=copy --output ..\madhonpub
 
 
 rem dotnet publish --no-restore --no-build --configuration Release --self-contained=false --output ..\madhonpub
