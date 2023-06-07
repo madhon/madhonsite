8812ceffe0420098da45a3728b49181ea4937e4f
Madhon <madhon@madhon.co.uk>
2023-06-07 14:21:11 +0100

    fix: minor layout fix

--- Romulus.Web/Infrastructure/ContentDeliveryNetwork.cs
+++ Romulus.Web/Infrastructure/ContentDeliveryNetwork.cs
@@ -1,39 +1,43 @@
-﻿namespace Romulus.Web.Infrastructure
+﻿namespace Romulus.Web.Infrastructure;
+
+#pragma warning disable S1075 // URIs should not be hardcoded
+public static class ContentDeliveryNetwork
 {
-    public static class ContentDeliveryNetwork
-    {
-        public static class Google
-        {
-            public const string Domain = "ajax.googleapis.com";
-            public const string JQuery3Url = "https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js";
-        }
+	public static class Google
+	{
+		public const string Domain = "ajax.googleapis.com";
 
-        public static class JsDelivrCdn
-        {
-            public const string Domain = "cdn.jsdelivr.net";
+		public const string JQuery3Url = "https://ajax.googleapis.com/ajax/libs/jquery/3.7.0/jquery.min.js";
 
-            public const string BootstrapCssUrl =
-              "https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css";
+	}
 
-            public const string BootstrapJsUrl = "https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js";
-        }
+	public static class JsDelivrCdn
+	{
+		public const string Domain = "cdn.jsdelivr.net";
 
-        public static class Cloudflare
-        {
-            public const string Domain = "cdnjs.cloudflare.com";
+		public const string BootstrapCssUrl =
+		  "https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css";
 
-            public const string ModernizrUrl = "https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js";
+		public const string BootstrapJsUrl = "https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js";
+	}
 
-            public const string ParsleyUrl = "https://cdnjs.cloudflare.com/ajax/libs/parsley.js/2.9.2/parsley.min.js";
+	public static class Cloudflare
+	{
+		public const string Domain = "cdnjs.cloudflare.com";
 
-            public const string Migrate3Url =
-              "https://cdnjs.cloudflare.com/ajax/libs/jquery-migrate/3.4.1/jquery-migrate.min.js";
-        }
+		public const string ModernizrUrl = "https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js";
 
-        public static class UnPkg
-        {
-	        public const string Domain = "unpkg.com";
-	        public const string Popper = "https://unpkg.com/@popperjs/core@2.11.7/dist/umd/popper.min.js";
-        }
-    }
+		public const string ParsleyUrl = "https://cdnjs.cloudflare.com/ajax/libs/parsley.js/2.9.2/parsley.min.js";
+
+		public const string Migrate3Url =
+		  "https://cdnjs.cloudflare.com/ajax/libs/jquery-migrate/3.4.1/jquery-migrate.min.js";
+	}
+
+	public static class UnPkg
+	{
+			public const string Domain = "unpkg.com";
+			public const string Popper = "https://unpkg.com/@popperjs/core@2.11.7/dist/umd/popper.min.js";
+	}
 }
+#pragma warning restore S1075 // URIs should not be hardcoded
+
