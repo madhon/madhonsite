48f61e53970d4a1766395fc66d9366dba11a570c
Madhon <madhon@madhon.co.uk>
2025-10-23 09:53:53 +0100

    feat: move to .net 10 ..maybe

--- Romulus.Web/Program.cs
+++ Romulus.Web/Program.cs
@@ -15,7 +15,7 @@ builder.Host.UseSystemd();
 builder.Services.Configure<ForwardedHeadersOptions>(options =>
 {
     options.ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto;
-    options.KnownNetworks.Clear();
+    options.KnownIPNetworks.Clear();
     options.KnownProxies.Clear();
 });
 
